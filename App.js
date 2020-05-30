ec4be200fad6fd94759581b2df4b71cd8af3225b
KuWathsala <wathdanthasinghe@gmail.com>
2020-05-30 07:19:02 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Components/Counter';
+import {Provider} from 'react-redux';
+import store from './Redux/store';
+
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <Provider store={store}>
+        <Counter/>
+      </Provider>      
+    );
+  }
+} 
